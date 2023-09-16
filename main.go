e33ab8d9b26fcd841f34b330c6ada45bb9f9a7b0
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-09-17 00:13:59 +0100

    Updating Port

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"os"
+
 	"github.com/LordEwan/tiktok-trends/internal/fetch"
 	"github.com/gocolly/colly"
 	"github.com/gofiber/fiber/v2"
 )
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":0000"
+	} else {
+		port = ":" + port
+	}
+
+	return port
+}
 
 func main() {
 	app := fiber.New()
@@ -15,5 +28,5 @@ func main() {
 		return c.JSON(arr)
 	})
 
-	app.Listen(":0000")
+	app.Listen(getPort())
 }
